19d90a84d6a130c2f3c5f6b3035769f8b115354a
ReeceStevens <reecestevens@utexas.edu>
2014-04-16 22:12:00 -0500

    Can now choose an email address to send results to.

--- app/src/main/java/ut/ewh/audiometrytest/ExportData.java
+++ app/src/main/java/ut/ewh/audiometrytest/ExportData.java
@@ -1,14 +1,12 @@
 package ut.ewh.audiometrytest;
 
+import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.util.Log;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
+import android.view.Menu;
+import android.view.View;
+import android.view.MenuItem;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
@@ -17,8 +15,17 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import android.widget.EditText;
+import android.widget.Button;
+
 
 public class ExportData extends ActionBarActivity {
+    Button doneButton;
+    EditText email_field;
+    String email = "no email has yet been entered into this ridiculously long initialized field";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +33,48 @@ public class ExportData extends ActionBarActivity {
         setContentView(R.layout.activity_export_data);
         ActionBar actionbar = getSupportActionBar();
         actionbar.setDisplayHomeAsUpEnabled(true);
-        try{
+        doneButton = (Button)findViewById(R.id.doneButton);
+        email_field = (EditText)findViewById(R.id.email_address);
+        /**
+         * Takes entered email address and sends test results to that email
+         */
+        doneButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                email = email_field.getText().toString();
+                try{
+                    // super.onCreate(savedInstanceState);
+                    //setContentView(R.layout.activity_export_data);
+                    HttpClient httpclient = new DefaultHttpClient();
+                    String URL = "http://107.170.226.198/mail.php?";
+                    URL += "t="+ email;
+                    URL += "&s="+"test";
+                    URL += "&b="+"body";
+                    HttpResponse response = httpclient.execute(new HttpGet(URL));
+                    StatusLine statusLine = response.getStatusLine();
+                    if (statusLine.getStatusCode() == HttpStatus.SC_OK){
+                        ByteArrayOutputStream out = new ByteArrayOutputStream();
+                        response.getEntity().writeTo(out);
+                        out.close();
+                        String responseString = out.toString();
+                        Log.i("Connection", "Successful");
+                        Log.i("connection", responseString);
+                    }  else {
+                        Log.i("connection", "unsuccessful");
+                        response.getEntity().getContent().close();
+                        throw new IOException(statusLine.getReasonPhrase());
+                    }
+                } catch (Exception e){
+                    Log.i("Not Good", "Not Pinging IP");
+                    System.err.println(e);
+                }
+            }
+        });
+        /*try{
            // super.onCreate(savedInstanceState);
             //setContentView(R.layout.activity_export_data);
             HttpClient httpclient = new DefaultHttpClient();
             String URL = "http://107.170.226.198/mail.php?";
-            URL += "t="+"reeceman524@gmail.com";
+            URL += "t="+email;
             URL += "&s="+"test";
             URL += "&b="+"body";
             HttpResponse response = httpclient.execute(new HttpGet(URL));
@@ -51,7 +94,7 @@ public class ExportData extends ActionBarActivity {
         } catch (Exception e){
             Log.i("Not Good", "Not Pinging IP");
             System.err.println(e);
-        }
+        }*/
     }
 
 
