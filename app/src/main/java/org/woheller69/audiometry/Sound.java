95eb8eb4a2bf573271eac4a62079642c76f45f82
woheller69 <wolfgang.heller@web.de>
2022-05-07 19:34:47 +0200

    added currently unused sound methods

--- app/src/main/java/org/woheller69/audiometry/Sound.java
+++ app/src/main/java/org/woheller69/audiometry/Sound.java
@@ -36,4 +36,39 @@ public class Sound {
         audioTrack.play();
         return audioTrack;
     }
+
+    /**
+     * Generates the tone based on the increment and volume, used in inner loop
+     * @param increment - the amount to increment by
+     * @param volume - the volume to generate
+     */
+    public float[] genStereoTone(float increment, int volume, int numSamples, int ear){
+
+        float angle = 0;
+        float[] generatedSnd = new float[2*numSamples];
+        for (int i = 0; i < numSamples; i=i+2){
+            if (ear == 0) {
+                generatedSnd[i] = (float) (Math.sin(angle)*volume/32768);
+                generatedSnd[i+1] = 0;
+            } else {
+                generatedSnd[i] = 0;
+                generatedSnd[i+1] = (float) (Math.sin(angle)*volume/32768);
+            }
+            angle += increment;
+        }
+        return generatedSnd;
+    }
+
+    /**
+     * Writes the parameter byte array to an AudioTrack and plays the array
+     * @param generatedSnd- input PCM float array
+     * PROBLEM: On some devices sound from one stereo channel (as created by genStereoSound) can be heard on the other channel
+     */
+    public AudioTrack playStereoSound(float[] generatedSnd, int sampleRate) {
+        AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, sampleRate, AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_FLOAT, generatedSnd.length, AudioTrack.MODE_STATIC);
+        audioTrack.write(generatedSnd,0,generatedSnd.length,AudioTrack.WRITE_BLOCKING);
+        audioTrack.setVolume(AudioTrack.getMaxVolume());
+        audioTrack.play();
+        return audioTrack;
+    }
 }
